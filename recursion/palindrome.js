d20cf7c4261827b996190640f982bcf99b846237
visaj-555 <visajpanchal777@gmail.com>
2025-07-09 09:03:13 +0530

    09-07-2025

--- recursion/palindrome.js
+++ recursion/palindrome.js
@@ -1,3 +1,5 @@
+// Brute force appraoch
+
 function isPalindrome(num) {
   numStr = num.toString();
   reverseNum = numStr.split("").reverse().join("");
@@ -10,3 +12,23 @@ function isPalindrome(num) {
 }
 
 console.log(isPalindrome(121));
+
+// Recursive Approach
+
+console.log("Recursion Approach : ");
+
+function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  if (str.length <= 1) {
+    return true;
+  }
+
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
+
+  return isPalindrome(str.slice(1, str.length - 1));
+}
+
+console.log(isPalindrome("racecar"));
--- recursion/sum.js
+++ recursion/sum.js
@@ -6,4 +6,21 @@ function sumTo(n) {
   }
 }
 
-console.log(sumTo(55));
+console.log(sumTo(5));
+
+// Call Stack Explanation
+
+function sumTo(n) {
+  console.log("Calling sumTo(" + n + ")");
+
+  if (n <= 1) {
+    console.log("Reached base case: return 1");
+    return 1;
+  }
+
+  const result = n + sumTo(n - 1);
+  console.log("Returning: " + result + " from sumTo(" + n + ")");
+  return result;
+}
+
+console.log("Final Answer:", sumTo(5));
