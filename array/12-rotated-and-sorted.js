f7da6a5820ab713abc511e88c837f7ecff59e2bb
visaj-555 <visajpanchal777@gmail.com>
2025-08-05 08:53:39 +0530

    05-08-2025

---
+++ array/12-rotated-and-sorted.js
@@ -0,0 +1,19 @@
+var check = function (nums) {
+  let count = 0;
+  let n = nums.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      count++;
+    }
+  }
+
+  // Only check once after the loop
+  if (nums[n - 1] > nums[0]) {
+    count++;
+  }
+
+  return count <= 1;
+};
+
+console.log(checkArray([3, 4, 5, 1, 2]));
