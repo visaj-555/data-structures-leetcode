8733a48973182af0d8701bcd8bc7b5fe7706b750
visaj-555 <visajpanchal777@gmail.com>
2025-08-11 09:44:26 +0530

    11-08-2025

---
+++ array/17-union.js
@@ -0,0 +1,19 @@
+function Union(nums1, nums2) {
+  let nums3 = [];
+
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] == nums2[j]) {
+        nums3.push(nums1[i]);
+      }
+    }
+  }
+  return nums3;
+}
+
+console.log(
+  Union(
+    [20, 30, 40, 10, 50, 100, 3000, 40000],
+    [10, 20, 30, 50, 40, 90, 50000, 60000]
+  )
+);
