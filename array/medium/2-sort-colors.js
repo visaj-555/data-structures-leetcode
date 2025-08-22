03217a7dd2bf6712868d7182c7d74fe0bf6d0e63
visaj-555 <visajpanchal777@gmail.com>
2025-08-22 10:12:02 +0530

    22-08-2025

---
+++ array/medium/2-sort-colors.js
@@ -0,0 +1,22 @@
+function sortColors(nums) {
+  let low = 0,
+    mid = 0,
+    high = nums.length - 1;
+
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      [nums[low], nums[mid]] = [nums[mid], nums[low]];
+      low++;
+      mid++;
+    } else if (nums[mid] === 1) {
+      mid++;
+    } else {
+      [nums[mid], nums[high]] = [nums[high], nums[mid]];
+      high--;
+    }
+  }
+  return nums;
+}
+
+console.log(sortColors([2, 0, 2, 1, 1, 0])); // [0,0,1,1,2,2]
+console.log(sortColors([2, 0, 1])); // [0,1,2]
