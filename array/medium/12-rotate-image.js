7c0d12b400e723ad349f471dae7c702c20e600fe
visaj-555 <visajpanchal777@gmail.com>
2025-09-02 22:15:01 +0530

    02-09-2025

---
+++ array/medium/12-rotate-image.js
@@ -0,0 +1,25 @@
+// 48. Rotate Image
+
+var rotate = function (matrix) {
+  const n = matrix.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    matrix[i].reverse();
+  }
+};
+
+console.log(
+  rotate([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
