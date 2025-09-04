3732ab4036c9bffbbf5facb52d13e37aad8ae259
visaj-555 <visajpanchal777@gmail.com>
2025-09-04 22:41:27 +0530

    04-09-2025

---
+++ array/medium/14-subarray-equals-k.js
@@ -0,0 +1,19 @@
+var subarraySum = function (nums, k) {
+  const map = new Map();
+  map.set(0, 1);
+
+  let prefixSum = 0;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    prefixSum += nums[i];
+
+    if (map.has(prefixSum - k)) {
+      count += map.get(prefixSum - k);
+    }
+
+    map.set(prefixSum, (map.get(prefixSum) || 0) + 1);
+  }
+
+  return count;
+};
