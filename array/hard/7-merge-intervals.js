fc21f65589e86ca23c62d501f1f3d7f7c29e5be8
visaj-555 <visajpanchal777@gmail.com>
2025-11-06 22:26:23 +0530

    06-11-2025

---
+++ array/hard/7-merge-intervals.js
@@ -0,0 +1,27 @@
+function merge(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let merged = [];
+
+  for (let i = 0; i < intervals.length; i++) {
+    if (merged.length === 0 || merged[merged.length - 1][1] < intervals[i][0]) {
+      merged.push(intervals[i]);
+    } else {
+      merged[merged.length - 1][1] = Math.max(
+        merged[merged.length - 1][1],
+        intervals[i][1]
+      );
+    }
+  }
+
+  return merged;
+}
+
+console.log(
+  merge([
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ])
+);
