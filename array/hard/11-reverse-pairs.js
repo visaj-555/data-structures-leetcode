a1dc58b593e232d45ddb3c49945fd0f2c9a476d8
visaj-555 <visajpanchal777@gmail.com>
2025-11-10 21:33:39 +0530

    10-11-2025

--- array/hard/11-reverse-pairs.js
+++ array/hard/11-reverse-pairs.js
@@ -1,15 +1,41 @@
 var reversePairs = function (nums) {
-  let count = 0;
+  function mergeSort(arr, low, high) {
+    if (low >= high) return 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] > 2 * nums[j]) {
-        count++;
-      }
+    let mid = Math.floor((low + high) / 2);
+    let count = mergeSort(arr, low, mid) + mergeSort(arr, mid + 1, high);
+    count += countPairs(arr, low, mid, high);
+    merge(arr, low, mid, high);
+    return count;
+  }
+
+  function countPairs(arr, low, mid, high) {
+    let count = 0;
+    let j = mid + 1;
+    for (let i = low; i <= mid; i++) {
+      while (j <= high && arr[i] > 2 * arr[j]) j++;
+      count += j - (mid + 1);
+    }
+    return count;
+  }
+
+  function merge(arr, low, mid, high) {
+    let temp = [];
+    let left = low,
+      right = mid + 1;
+    while (left <= mid && right <= high) {
+      if (arr[left] <= arr[right]) temp.push(arr[left++]);
+      else temp.push(arr[right++]);
+    }
+    while (left <= mid) temp.push(arr[left++]);
+    while (right <= high) temp.push(arr[right++]);
+
+    for (let i = low; i <= high; i++) {
+      arr[i] = temp[i - low];
     }
   }
 
-  return count;
+  return mergeSort(nums, 0, nums.length - 1);
 };
 
 console.log(reversePairs([1, 3, 2, 3, 1]));
