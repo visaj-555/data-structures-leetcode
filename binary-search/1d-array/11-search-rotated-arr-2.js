d427e98cee7715411fb5e73bfc8be500bd892576
visaj-555 <visajpanchal777@gmail.com>
2025-11-19 22:04:52 +0530

    19-11-2025

---
+++ binary-search/1d-array/11-search-rotated-arr-2.js
@@ -0,0 +1,41 @@
+// 81. Search in Rotated Sorted Array II
+
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return true;
+    }
+
+    if (nums[left] === nums[mid] && nums[mid] === nums[right]) {
+      left++;
+      right--;
+      continue;
+    }
+
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return false;
+};
+
+console.log(search([2, 5, 6, 0, 0, 1, 2], 0));
+console.log(search([2, 5, 6, 0, 0, 1, 2], 3));
+console.log(search([1, 1, 1, 1, 1], 1));
+console.log(search([1, 0, 1, 1, 1], 0));
