38fb5bf707bf3cb0bfc4e072ecb4ab2ae99988f5
visaj-555 <visajpanchal777@gmail.com>
2025-11-22 22:59:09 +0530

    22-11-2025

---
+++ binary-search/1d-array/15-single-sorted-arr.js
@@ -0,0 +1,23 @@
+// 540. Single Element in a Sorted Array
+
+var singleNonDuplicate = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (mid % 2 === 1) mid--;
+
+    if (nums[mid] === nums[mid + 1]) {
+      left = mid + 2;
+    } else {
+      right = mid;
+    }
+  }
+
+  return nums[left];
+};
+
+console.log(singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8]));
+console.log(singleNonDuplicate([3, 3, 7, 7, 10, 11, 11]));
