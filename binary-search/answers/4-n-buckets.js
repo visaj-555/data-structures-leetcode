fc63508fd74da1ad3f2661f014aee7ba858db57b
visaj-555 <visajpanchal777@gmail.com>
2025-11-28 08:49:58 +0530

    28-11-2025

---
+++ binary-search/answers/4-n-buckets.js
@@ -0,0 +1,41 @@
+var minDays = function (bloomDay, m, k) {
+  if (m * k > bloomDay.length) return -1;
+
+  const canMakeBouquets = (day) => {
+    let bouquets = 0;
+    let flowers = 0;
+
+    for (let bloom of bloomDay) {
+      if (bloom <= day) {
+        flowers++;
+        if (flowers === k) {
+          bouquets++;
+          flowers = 0;
+        }
+      } else {
+        flowers = 0;
+      }
+    }
+
+    return bouquets >= m;
+  };
+
+  let left = Math.min(...bloomDay);
+  let right = Math.max(...bloomDay);
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (canMakeBouquets(mid)) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+};
+
+console.log(minDays([1, 10, 3, 10, 2], 3, 1));
+console.log(minDays([1, 10, 3, 10, 2], 3, 2));
+console.log(minDays([7, 7, 7, 7, 12, 7, 7], 2, 3));
