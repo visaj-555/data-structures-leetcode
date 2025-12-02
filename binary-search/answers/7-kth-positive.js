586467b4643f08ca0beb06394aea6a4e7c6e844d
visaj-555 <visajpanchal777@gmail.com>
2025-12-02 22:17:09 +0530

    02-12-2025

---
+++ binary-search/answers/7-kth-positive.js
@@ -0,0 +1,19 @@
+var findKthPositive = function (arr, k) {
+  let num = 1;
+  let i = 0;
+
+  while (k > 0) {
+    if (i < arr.length && arr[i] === num) {
+      i++;
+    } else {
+      k--;
+      if (k === 0) return num;
+    }
+    num++;
+  }
+
+  return num;
+};
+
+console.log(findKthPositive([2, 3, 4, 7, 11], 5));
+console.log(findKthPositive([1, 2, 3, 4], 2));
