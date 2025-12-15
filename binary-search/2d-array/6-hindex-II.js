9d38b6fea9652655a8f56cfb35745b391b3fae72
visaj-555 <visajpanchal777@gmail.com>
2025-12-15 22:39:44 +0530

    15-12-2025

---
+++ binary-search/2d-array/6-hindex-II.js
@@ -0,0 +1,23 @@
+var hIndex = function (citations) {
+  let n = citations.length;
+  let left = 0,
+    right = n - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let h = n - mid;
+
+    if (citations[mid] >= h) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return n - left;
+};
+
+console.log(hIndex([0, 1, 3, 5, 6]));
+console.log(hIndex([1, 2, 100]));
+console.log(hIndex([0, 0, 0]));
+console.log(hIndex([10, 10, 10]));
