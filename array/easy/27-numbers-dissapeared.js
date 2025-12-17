43db3d01db74cf88b40b56ab5bc551ae25d7038c
visaj-555 <visajpanchal777@gmail.com>
2025-12-17 22:51:27 +0530

    17-12-2025

---
+++ array/easy/27-numbers-dissapeared.js
@@ -0,0 +1,19 @@
+var findDisappearedNumbers = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let index = Math.abs(nums[i]) - 1;
+    if (nums[index] > 0) {
+      nums[index] = -nums[index];
+    }
+  }
+
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+
+  return result;
+};
+
+console.log(findDisappearedNumbers[(-4, -3, -2, -7, 8, 2, -3, -1)]);
