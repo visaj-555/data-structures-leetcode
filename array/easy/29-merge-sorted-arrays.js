7dee093cbfc4665225ff7786e3f8e1a60b8e3213
visaj-555 <visajpanchal777@gmail.com>
2026-01-09 21:34:50 +0530

    09-01-2025

---
+++ array/easy/29-merge-sorted-arrays.js
@@ -0,0 +1,22 @@
+// 88. Merge Sorted Array
+
+var merge = function (nums1, m, nums2, n) {
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let p = m + n - 1;
+
+  while (p2 >= 0) {
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[p] = nums1[p1];
+      p1--;
+    } else {
+      nums1[p] = nums2[p2];
+      p2--;
+    }
+    p--;
+  }
+};
+
+console.log(((a) => (merge(a, 3, [2, 5, 6], 3), a))([1, 2, 3, 0, 0, 0]));
+console.log(((a) => (merge(a, 1, [], 0), a))([1]));
+console.log(((a) => (merge(a, 0, [1], 1), a))([0]));
