f332a5b00e02a078706bf238fea59b379b01fd24
visaj-555 <visajpanchal777@gmail.com>
2026-03-30 15:25:06 +0530

    Revised Next Permutation

--- array/medium/8-next-permutation.js
+++ array/medium/8-next-permutation.js
@@ -1,23 +1,24 @@
 // 31. Next Permutation
 
 var nextPermutation = function (nums) {
-  let i = nums.length - 2;
-  while (i >= 0 && nums[i] >= nums[i + 1]) {
-    i--;
-  }
+  let n = nums.length;
 
+  // 1. BREAK → find dip
+  let i = n - 2;
+  while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+
+  // 2. SWAP → just bigger element
   if (i >= 0) {
-    let j = nums.length - 1;
-
+    let j = n - 1;
     while (nums[j] <= nums[i]) {
       j--;
     }
-
     [nums[i], nums[j]] = [nums[j], nums[i]];
   }
 
-  let left = i + 1;
-  let right = nums.length - 1;
+  // 3. FIX → reverse right side
+  let left = i + 1,
+    right = n - 1;
   while (left < right) {
     [nums[left], nums[right]] = [nums[right], nums[left]];
     left++;
