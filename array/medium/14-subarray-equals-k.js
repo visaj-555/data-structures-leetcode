4ed940e941e9916da65482f225affe093cab7133
visaj-555 <visajpanchal777@gmail.com>
2026-04-06 19:09:39 +0530

    Added Brute force for Sub Array equals k

--- array/medium/14-subarray-equals-k.js
+++ array/medium/14-subarray-equals-k.js
@@ -1,5 +1,27 @@
 //  560. Subarray Sum Equals K
 
+// Brute Force :
+
+function subarraySum(nums, k) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+
+      if (sum === k) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+// Optimal :
+
 var subarraySum = function (nums, k) {
   const map = new Map();
   map.set(0, 1);
@@ -19,3 +41,5 @@ var subarraySum = function (nums, k) {
 
   return count;
 };
+
+console.log(subarraySum([1, 1, 1], 2));
