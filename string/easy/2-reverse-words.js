d0358adf5a959d4f8b4855562f888b237e1a7f1c
visaj-555 <visajpanchal777@gmail.com>
2026-04-07 09:00:31 +0530

    Brute Force of Reverse words

--- string/easy/2-reverse-words.js
+++ string/easy/2-reverse-words.js
@@ -1,5 +1,21 @@
 // 151. Reverse Words in a String
 
+// Brute Force :
+var reverseWords = function (s) {
+  let str = s.split(" ");
+  let rev = [];
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i]) {
+      rev.push(str[i]);
+    }
+  }
+
+  return rev.join(" ");
+};
+
+// Simpler :
+
 function reverseWords(s) {
   return s.trim().split(/\s+/).reverse().join(" ");
 }
