2c20a66e91ffd2f362c1f3addd55d1d77107fd33
visaj-555 <visajpanchal777@gmail.com>
2026-06-20 18:48:43 +0530

    Added Optimal Solution for Majority-Element-II

--- array/hard/2-majority-element.js
+++ array/hard/2-majority-element.js
@@ -1,4 +1,7 @@
 // 229. Majority Element II
+
+// Hashmap :
+
 var majorityElement = function (nums) {
   const count = {};
   const threshold = Math.floor(nums.length / 3);
@@ -13,6 +16,53 @@ var majorityElement = function (nums) {
       result.push(Number(num));
     }
   }
+
+  return result;
+};
+
+// Optimal : Boyer Moore Voting :
+
+var majorityElement = function (nums) {
+  let candidate1 = null;
+  let candidate2 = null;
+  let count1 = 0;
+  let count2 = 0;
+
+  // First Pass: Find potential candidates
+  for (let num of nums) {
+    if (num === candidate1) {
+      count1++;
+    } else if (num === candidate2) {
+      count2++;
+    } else if (count1 === 0) {
+      candidate1 = num;
+      count1 = 1;
+    } else if (count2 === 0) {
+      candidate2 = num;
+      count2 = 1;
+    } else {
+      count1--;
+      count2--;
+    }
+  }
+
+  // Second Pass: Verify candidates
+  count1 = 0;
+  count2 = 0;
+
+  for (let num of nums) {
+    if (num === candidate1) {
+      count1++;
+    } else if (num === candidate2) {
+      count2++;
+    }
+  }
+
+  const result = [];
+  const threshold = Math.floor(nums.length / 3);
+
+  if (count1 > threshold) result.push(candidate1);
+  if (count2 > threshold) result.push(candidate2);
 
   return result;
 };
