a78b8d92e142a8c4c3b3de651198efb2ce55aecb
visaj-555 <visajpanchal777@gmail.com>
2026-07-15 07:50:04 +0530

    Added Easy Solution for Smallest divisors

--- binary-search/answers/5-smallest-divisors.js
+++ binary-search/answers/5-smallest-divisors.js
@@ -1,29 +1,28 @@
 // 1283. Find the Smallest Divisor Given a Threshold
 
-var smallestDivisor = function (nums, threshold) {
-  const calculateSum = (divisor) => {
-    return nums.reduce((sum, num) => {
-      return sum + Math.ceil(num / divisor);
-    }, 0);
-  };
+function getSum(nums, divisor) {
+  let sum = 0;
+  for (let num of nums) {
+    sum += Math.ceil(num / divisor);
+  }
+  return sum;
+}
 
+var smallestDivisor = function (nums, threshold) {
   let left = 1;
   let right = Math.max(...nums);
-  let result = right;
 
-  while (left <= right) {
+  while (left < right) {
     let mid = Math.floor((left + right) / 2);
-    let sum = calculateSum(mid);
 
-    if (sum <= threshold) {
-      result = mid;
-      right = mid - 1;
+    if (getSum(nums, mid) <= threshold) {
+      right = mid;
     } else {
       left = mid + 1;
     }
   }
 
-  return result;
+  return left;
 };
 
 console.log(smallestDivisor([1, 2, 5, 9], 6));
