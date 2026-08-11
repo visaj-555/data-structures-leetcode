1e13a7a9448fe9507c67bf91e3a9c2dafc8958f3
visaj-555 <visajpanchal777@gmail.com>
2026-08-11 12:13:05 +0530

    Simpler Solution

--- namaste-dsa-sheet/string/easy/1-length-last-word.js
+++ namaste-dsa-sheet/string/easy/1-length-last-word.js
@@ -1,55 +1,14 @@
 // 58. Length of Last Word
 
-
-// Easy Approach for 2 loops :
-
 var lengthOfLastWord = function (s) {
-    let n = s.length - 1;
-    let count = 0;
-
-    // skip trailing spaces
-    while (n >= 0 && s[n] === " ") {
-        n--;
+  let count = 0;
+  s = s.trim();
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] == " " || "") {
+      break;
+    } else {
+      count++;
     }
-
-    // count last word characters
-    while (n >= 0 && s[n] !== " ") {
-        count++;
-        n--;
-    }
-
-    return count;
-};
-
-// Single While Loop : 
-
-var lengthOfLastWord = function(s) {
-    let n = s.length - 1;
-    let count = 0;
-
-    while (n >= 0) {
-        if (s[n] !== " ") {
-            count++;
-        } else if (count > 0) {
-            break;
-        }
-        n--;
-    }
-
-    return count;
-};
-
-// Single For Loop : 
-
-var lengthOfLastWord = function(s) {
-    let count = 0;
-
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (s[i] !== ' ') {
-            count++;
-        } else if (count > 0) {
-            break;
-        }
-    }
-    return count;
+  }
+  return count;
 };
