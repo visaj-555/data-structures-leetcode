8a161251b02beadd0a970a402e398b8fb6c7c6d9
visaj-555 <visajpanchal777@gmail.com>
2026-08-19 16:46:33 +0530

    Added 2 Approaches for Group Anagrams

--- namaste-dsa-sheet/string/medium/1-group-anagrams.js
+++ namaste-dsa-sheet/string/medium/1-group-anagrams.js
@@ -1,25 +1,47 @@
 // 49. Group Anagrams
 
-
-// Easy Solution : 
+// Approach 1 : Sorted Key
 var groupAnagrams = function (strs) {
-  const map = new Map(); // Step 1: create empty map
+  let map = {};
 
-  for (let word of strs) {
-    // Step 2: loop each word
+  for (let i = 0; i < strs.length; i++) {
+    let sortedStr = strs[i].split("").sort().join("");
 
-    // Step 3: create key by sorting
-    let key = word.split("").sort().join("");
-
-    // Step 4: if key not present → create new group
-    if (!map.has(key)) {
-      map.set(key, []);
+    if (!map[sortedStr]) {
+      map[sortedStr] = [strs[i]];
+    } else {
+      map[sortedStr].push(strs[i]);
     }
-
-    // Step 5: push word into its group
-    map.get(key).push(word);
   }
 
-  // Step 6: return only values (groups)
-  return Array.from(map.values());
+  return Object.values(map);
+};
+
+// Approach 2 : Hashed Key
+
+var groupAnagrams = function (strs) {
+  let map = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    let freqArr = Array(26).fill(0);
+    let s = strs[i];
+
+    for (let j = 0; j < s.length; j++) {
+      let index = s[j].charCodeAt(0) - "a".charCodeAt(0);
+      freqArr[index]++;
+    }
+
+    let key = "";
+    for (let k = 0; k < 26; k++) {
+      key += "#" + freqArr[k];
+    }
+
+    if (!map[key]) {
+      map[key] = [s];
+    } else {
+      map[key].push(s);
+    }
+  }
+
+  return Object.values(map);
 };
