de28237020d9b6f01513703e5d92375467b19fa3
visaj-555 <visajpanchal777@gmail.com>
2026-09-08 07:06:33 +0530

    Added Easy Solution : Linked List Cycle

--- namaste-dsa-sheet/linked-list/4-linked-list-cycle.js
+++ namaste-dsa-sheet/linked-list/4-linked-list-cycle.js
@@ -1,32 +1,34 @@
- // 141. Linked List Cycle 
+// 141. Linked List Cycle
 
 //  Approach 1 : Hash Table
 
-var hasCycle = function(head) {
-    let seenNodes = new Set();
-    let curr = head;
-    while (curr !== null) {
-        if (seenNodes.has(curr)) {
-            return true;
-        }
-        seenNodes.add(curr);
-        curr = curr.next;
+var hasCycle = function (head) {
+  let seenNodes = new Set();
+  let curr = head;
+  while (curr !== null) {
+    if (seenNodes.has(curr)) {
+      return true;
     }
-    return false;
+    seenNodes.add(curr);
+    curr = curr.next;
+  }
+  return false;
 };
-
 
 // Approach 2  : Floyd’s Cycle Detection
 
-var hasCycle = function(head) {
-    if (!head) return false;
-    let slow = head;
-    let fast = head.next;
-    while (slow !== fast) {
-        if (!fast || !fast.next) return false;
-        slow = slow.next;
-        fast = fast.next.next;
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
     }
-    return true;
+  }
+
+  return false;
 };
-   
